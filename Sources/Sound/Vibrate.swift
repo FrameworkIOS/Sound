09e67607d9699342087de604b45da72df073eaa3
krishpranav <krisna.pranav@gmail.com>
2023-03-04 12:28:02 +0530

    Sound: Vibrate[vibrate codes]

---
+++ Sources/Sound/Vibrate.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by eCOM-Elango.a on 04/03/23.
+//
+
+import Foundation
+
+@available(iOS 10.0, *)
+extension Sound {
+    public enum Vibration: Int {
+        case `default` = 4085
+        
+        /**
+         - Alert
+         */
+        case alert = 1011
+    }
+}
