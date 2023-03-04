056a550e24293ae806304628b8857b433f4919d2
krishpranav <krisna.pranav@gmail.com>
2023-03-04 13:05:56 +0530

    Sound: TapticEngine

--- Sources/Sound/TapticEngine.swift
+++ Sources/Sound/TapticEngine.swift
@@ -9,7 +9,15 @@ import Foundation
 
 @available(iOS 10.0, *)
 extension Sound {
-    public enum TapticEngine {
+    public enum TapticEngine: Int {
+        case peek = 1519
         
+        case pop = 1520
+        
+        case cancelled = 1521
+        
+        case tryAgain = 1102
+        
+        case failed = 1107
     }
 }
